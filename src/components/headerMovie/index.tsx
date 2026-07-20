b723741de9b08505c7d9b22722b3e44fa92eb239
Ever David Beltran Pinto <your-email@example.com>
2026-07-19 23:26:00 -0500

    Exercise - Show favourite icon on Movie Details Page.

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,38 +1,63 @@
 import React from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import HomeIcon from "@mui/icons-material/Home";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import HomeIcon from "@mui/icons-material/Home";
-import { MovieDetailsProps } from "../../types/interfaces"; 
+import Avatar from "@mui/material/Avatar";
+import { MovieDetailsProps } from "../../types/interfaces";
 
 const styles = {
-    root: {  
+  root: {
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  },
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
-  
+  const favourites = JSON.parse(
+    localStorage.getItem("favourites") || "[]"
+  );
+
+  const isFavourite = favourites.some(
+    (favourite: number | string | MovieDetailsProps) => {
+      if (typeof favourite === "object") {
+        return Number(favourite.id) === Number(movie.id);
+      }
+
+      return Number(favourite) === Number(movie.id);
+    }
+  );
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
+      {isFavourite ? (
+        <Avatar sx={styles.avatar}>
+          <FavoriteIcon />
+        </Avatar>
+      ) : null}
+
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="large"/>
+          <HomeIcon color="primary" fontSize="large" />
         </a>
         <br />
         <span>{`${movie.tagline}`} </span>
       </Typography>
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
