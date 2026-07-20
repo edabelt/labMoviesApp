7c0d23f2648bb77ee76ab521a8017a7925fa1dec
Ever David Beltran Pinto <your-email@example.com>
2026-07-19 23:15:52 -0500

    Refactor: Template list page; HTTP API calls centralised.

--- src/components/filterMoviesCard/index.tsx
+++ src/components/filterMoviesCard/index.tsx
@@ -18,6 +18,7 @@ import Select from "@mui/material/Select";
 
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import SortIcon from "@mui/icons-material/Sort";
+import { getGenres } from "../../api/tmdb-api";
 
 const styles = {
   root: {
@@ -50,20 +51,13 @@ const FilterMoviesCard: React.FC<FilterMoviesCardProps> = ({
     { id: 0, name: "All" },
   ]);
 
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=${
-        import.meta.env.VITE_TMDB_KEY
-      }`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setGenres([
-          { id: 0, name: "All" },
-          ...json.genres,
-        ]);
-      });
-  }, []);
+    useEffect(() => {
+    getGenres().then((allGenres) => {
+      setGenres([genres[0], ...allGenres]);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement> | SelectChangeEvent<string>,
